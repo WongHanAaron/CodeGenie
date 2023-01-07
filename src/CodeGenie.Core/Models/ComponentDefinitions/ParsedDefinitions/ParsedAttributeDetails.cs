b69f9dd24d7a252d4fdde0dc99b652192a1de3cc
AaronWong <aaronwonghan@gmail.com>
2023-01-07 06:53:07 -0800

    Created new project for UI editor control

---
+++ src/CodeGenie.Core/Models/ComponentDefinitions/ParsedDefinitions/ParsedAttributeDetails.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeGenie.Core.Models.ComponentDefinitions.ParsedDefinitions
+{
+    public class ParsedAttributeDetails
+    {
+        public string Purpose { get; set; }
+    }
+}
