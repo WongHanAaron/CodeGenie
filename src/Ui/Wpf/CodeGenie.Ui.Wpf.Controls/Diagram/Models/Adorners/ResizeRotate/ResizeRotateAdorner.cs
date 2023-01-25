7994412c52f261b26e1976cd11214988db5913af
AaronWong <aaronwonghan@gmail.com>
2023-01-25 06:19:26 -0800

    Copied in resize rotate adorner

---
+++ src/Ui/Wpf/CodeGenie.Ui.Wpf.Controls/Diagram/Models/Adorners/ResizeRotate/ResizeRotateAdorner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace CodeGenie.Ui.Wpf.Controls.Diagram.Models.Adorners.ResizeRotate
+{
+    public class ResizeRotateAdorner : Adorner
+    {
+        private VisualCollection visuals;
+        private ResizeRotateChrome chrome;
+
+        protected override int VisualChildrenCount
+        {
+            get
+            {
+                return visuals.Count;
+            }
+        }
+
+        public ResizeRotateAdorner(ContentControl designerItem)
+            : base(designerItem)
+        {
+            SnapsToDevicePixels = true;
+            chrome = new ResizeRotateChrome();
+            chrome.DataContext = designerItem;
+            visuals = new VisualCollection(this);
+            visuals.Add(chrome);
+        }
+
+        protected override Size ArrangeOverride(Size arrangeBounds)
+        {
+            chrome.Arrange(new Rect(arrangeBounds));
+            return arrangeBounds;
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return visuals[index];
+        }
+    }
+}
