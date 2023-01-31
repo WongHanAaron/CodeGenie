0b392e03d517d4cb3831c1cd707d7a58bae8c989
AaronWong <aaronwonghan@gmail.com>
2023-01-31 08:00:53 -0800

    Created test project for CodeGenie.Ui.Wpf.Controls

---
+++ tests/CodeGenie.Ui.Wpf.Controls.Tests/CodeEditor/AutoComplete/CompletionDataSuggester_Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenie.Ui.Wpf.Controls.Tests.CodeEditor.AutoComplete
+{
+    public class CompletionDataSuggester_Tests
+    {
+    }
+}
---
+++ tests/CodeGenie.Ui.Wpf.Controls.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CodeGenie.Ui.Wpf.Controls.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
